ee5466268160ce0c7fbcfa54a4ef18539e18db96
Ruslan Karabalin <karabalin112@gmail.com>
2023-02-13 21:14:29 +0600

    [*]

--- cpp0/include/Box.h
+++ cpp0/include/Box.h
@@ -20,17 +20,89 @@ std::ostream& operator<<(std::ostream& os, Box box) {
     return os;
 }
 
-std::istream& operator>>(std::istream& os, Box box) {
-    os >> box.length >> box.width >> box.height >> box.weight >> box.value;
-    return os;
+Box operator>>(std::istream& is, Box& box) {
+    is >> box.length >> box.width >> box.height >> box.weight >> box.value;
+    return box;
 }
 
-Box create_box(int, int, int, double, int);
+Box create_box(int length, int width, int height, double weight, int value) {
+    Box box;
+    box.length = length;
+    box.width = width;
+    box.height = height;
+    box.weight = weight;
+    box.value = value;
+    return box;
+}
 
-int get_sum_value(Box[], int);
+int get_sum_value(Box boxes[], int size) {
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        sum += boxes[i].value;
+    }
+    return sum;
+}
 
-bool is_less_than(Box, int);
+bool is_less_than(Box box, int val) {
+    return (box.length + box.width + box.height <= val);
+}
 
-int get_max_weight(Box[], int, int);
+int get_max_weight(Box boxes[], int size, int maxV) {
+    int V;
+    int max = -1;
+    for(int i = 0; i < size; i++) {
+        V = boxes[i].length * boxes[i].width * boxes[i].height;
+        if (V <= maxV) {
+            if (max < boxes[i].weight) {
+                max = boxes[i].weight;
+            }
+        }
+    }
+    return max;
+}
 
-bool is_fit(Box[], int);
+bool is_fit(Box boxes[], int size) {
+    bool flag = true;
+
+    struct BoxV
+    {
+        Box box;
+        int V;
+    };
+
+    BoxV boxes_V[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        boxes_V[i].box = boxes[i];
+        boxes_V[i].V = boxes[i].length * boxes[i].width * boxes[i].height;
+    }
+    
+
+    for (int i = 0; i < size-1; i++) {
+        for (int j = 0; j < size-1-i; j++)
+        {
+            int _v = boxes_V[j].V;
+            if (_v > boxes_V[j+1].V)
+            {
+                BoxV _boxV = boxes_V[j];
+                boxes_V[j] = boxes_V[j+1];
+                boxes_V[j+1] = _boxV; 
+            }
+        }
+    }
+    
+    for (int i = 0; i < size-1; i++)
+    {
+        Box box1 = boxes_V[i].box;
+        Box box2 = boxes_V[i+1].box;
+
+        if (box1.length > box2.length || box1.width > box2.width || box1.height > box2.height)
+        {
+            flag = false;
+        }
+        
+    }
+
+    return flag;
+}
--- cpp0/main.cpp
+++ cpp0/main.cpp
@@ -1,8 +1,6 @@
 #include "./include/Box.h"
 
 int main() {
-    printf("Hello, World!\n");
-
     Box my_box0 = create_box(30, 30, 30, 10.0, 10000);
     Box my_box1 = create_box(20, 20, 20, 20.0, 20000);
     Box my_box2 = create_box(10, 10, 10, 30.0, 30000);
@@ -11,15 +9,15 @@ int main() {
 
     int sum = get_sum_value(boxes, 3);
 
-    printf("%d\n", sum);
+    printf("Sum: %d\n", sum);
 
-    printf("%d\n", is_less_than(my_box0, 40));
+    printf("Is less than: %d\n", is_less_than(my_box0, 40));
 
-    printf("%d\n", get_max_weight(boxes, 3, 8000));
+    printf("Get max weight: %d\n", get_max_weight(boxes, 3, 8000));
 
-    printf("%d\n", is_fit(boxes, 3));
+    printf("Is fit: %d\n", is_fit(boxes, 3));
 
-    printf("%d\n", my_box0 == my_box1);
+    printf("my_box1 == my_box2: %d\n", my_box0 == my_box1);
 
     Box cin_box;
 
